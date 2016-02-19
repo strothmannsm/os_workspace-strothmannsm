805d1b1109576db484b644054f53484f360c2588
Scott Strothmann <smsr3f@mail.missouri.edu>
2016-02-19 15:14:41 -0600

    Milestone 1

--- OSS16_Project1/src/process_scheduling.c
+++ OSS16_Project1/src/process_scheduling.c
@@ -14,6 +14,44 @@ void virtual_cpu(ProcessControlBlock_t* process_control_block) {
 }
 
 bool first_come_first_serve(dyn_array_t* ready_queue, ScheduleResult_t* result) {
+
+	if(!ready_queue || !result) {
+		return false;
+	}
+
+	size_t n_proc = dyn_array_size(ready_queue); //find out how many processes are in queue
+	uint32_t clocktime = 0; // initialize clocktime
+	float avg_latency = 0;
+	float avg_wallclock = 0;
+
+	/* While there are process in the ready queue
+	*  extract the PCB from the back and run it on the CPU
+	*  until it's done.
+	*/
+	while(dyn_array_size(ready_queue) > 0) {
+		ProcessControlBlock_t pcb;
+		if(dyn_array_extract_back(ready_queue, &pcb)) { // get next process out of the queue
+			avg_latency += clocktime; // process headed to cpu, add latency
+			// run process until completed
+			while(pcb.remaining_burst_time > 0) {
+				virtual_cpu(&pcb);
+				++clocktime; // another cycle has passed, increment
+			}
+			avg_wallclock += clocktime; // process finished, add wallclock
+		} else {
+			return false; // failed to extract pcb from queue
+		}
+	}
+
+	// average out latency and wallclock
+	avg_latency /= n_proc;
+	avg_wallclock /= n_proc;
+
+	// set timing results
+	result->average_latency_time = avg_latency;
+	result->average_wall_clock_time = avg_wallclock;
+	result->total_run_time = clocktime;
+
 	return true;
 }
 
